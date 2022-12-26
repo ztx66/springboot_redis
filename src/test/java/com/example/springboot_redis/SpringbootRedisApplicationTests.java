47c69431eee63f94fa41b7fa89c7d769373e1d6c
zhantianxin <2548449415@qq.com>
2022-12-26 17:15:20 +0800

    1.修改redis密码配置 2.编写RedisTemplate测试类

--- src/test/java/com/example/springboot_redis/SpringbootRedisApplicationTests.java
+++ src/test/java/com/example/springboot_redis/SpringbootRedisApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springboot_redis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class SpringbootRedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+
+       ValueOperations valueOperations= redisTemplate.opsForValue();
+
+       valueOperations.set("username","小一1");
+
     }
 
 }
